1702e62ee50e12e5e9d9834a8e127f6c069f92ce
Administrator <1105515654@qq.com>
2021-03-26 11:21:42 +0800

    添加logback日志处理

--- src/main/java/com/xgg/hightconcurren/threadpool/CacheThreadPoolTest.java
+++ src/main/java/com/xgg/hightconcurren/threadpool/CacheThreadPoolTest.java
@@ -1,5 +1,8 @@
 package com.xgg.hightconcurren.threadpool;
 
+import org.springframework.util.StringUtils;
+
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -14,8 +17,25 @@ public class CacheThreadPoolTest {
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newCachedThreadPool();
 
-        for (int i = 0; i < 1000; i++) {
+        /*for (int i = 0; i < 1000; i++) {
             executorService.execute(new Task());
-        }
+        }*/
+
+//        long i = 1 << 30;
+//        // 5 * 2³
+//        long k = 5 << 3;
+//        long j = 1073741824 >> 30;
+//
+//        int  p = 16 >> 4;
+//        System.out.println(i);
+//        System.out.println(k);
+//        System.out.println(j);
+//        System.out.println(p);
     }
+
+
+
+
+
+
 }
